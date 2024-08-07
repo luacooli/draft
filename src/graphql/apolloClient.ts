22b7e50adc3ec7c900b914d07277e250641f3a2d
luacooli <luacooli@gmail.com>
2024-08-07 16:25:42 -0300

    wip: pagination

--- src/graphql/apolloClient.ts
+++ src/graphql/apolloClient.ts
@@ -1,10 +1,26 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context';
 
 const client = new ApolloClient({
   link: new HttpLink({
     uri: 'https://api.tcgdex.net/v2/graphql',
   }),
   cache: new InMemoryCache(),
+})
+
+const queryParams = setContext((_, headers) => {
+  const params = {
+    'pagination:page': 1,
+    'pagination:itemsPerPage': 5 
+  }
+
+  const queryParams = new URLSearchParams(params).toString()
+
+  return {
+    headers: {
+      ...headers
+    }
+  }
 })
 
 export default client
